1cd72d527a4ea266157e485eef3f980b78278de7
Sachitha Abesinghe <sachithasandakelumsl@gmail.com>
2024-01-16 23:43:41 +0530

    Update tests with spring test

--- src/test/java/lk/ijse/dep11/edupanel/repository/custom/impl/LecturerRepositoryImplTest.java
+++ src/test/java/lk/ijse/dep11/edupanel/repository/custom/impl/LecturerRepositoryImplTest.java
@@ -1,5 +1,7 @@
 package lk.ijse.dep11.edupanel.repository.custom.impl;
 
+import lk.ijse.dep11.edupanel.WebAppConfig;
+import lk.ijse.dep11.edupanel.WebRootConfig;
 import lk.ijse.dep11.edupanel.entity.Lecturer;
 import lk.ijse.dep11.edupanel.repository.RepositoryFactory;
 import lk.ijse.dep11.edupanel.repository.custom.LecturerRepository;
@@ -7,22 +9,34 @@ import lk.ijse.dep11.edupanel.util.LecturerType;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
 
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+
+import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringJUnitWebConfig(classes = {WebAppConfig.class, WebRootConfig.class})
 class LecturerRepositoryImplTest {
 
     private final LecturerRepository repository = RepositoryFactory.getInstance()
             .getRepository(RepositoryFactory.RepositoryType.LECTURER);
     private EntityManager entityManager;
 
+    @Autowired
+    private EntityManagerFactory entityManagerFactory;
+
     @BeforeEach
     void setUp() {
-        entityManager = Persistence.createEntityManagerFactory("default")
-                .createEntityManager();
+        entityManager = entityManagerFactory.createEntityManager();
         repository.setEntityManager(entityManager);
         entityManager.getTransaction().begin();
     }
@@ -92,13 +106,47 @@ class LecturerRepositoryImplTest {
 
     @Test
     void findById() {
+        Lecturer lecturer = new Lecturer("Kasun Sampath",
+                "Senior Trainer",
+                "BSc (Hons) in Computing",
+                LecturerType.FULL_TIME,
+                0);
+        Lecturer savedLecturer = repository.save(lecturer);
+        Optional<Lecturer> optLecturer1 = repository.findById(savedLecturer.getId());
+        Optional<Lecturer> optLecturer2 = repository.findById(-2500);
+
+        assertTrue(optLecturer1.isPresent());
+        assertTrue(optLecturer2.isEmpty());
     }
 
     @Test
     void findAll() {
+        for (int i = 0; i < 8; i++) {
+            Lecturer lecturer = new Lecturer("Kasun Sampath",
+                    "Senior Trainer",
+                    "BSc (Hons) in Computing",
+                    LecturerType.FULL_TIME,
+                    0);
+            repository.save(lecturer);
+        }
+        List<Lecturer> lecturerList = repository.findAll();
+
+        assertEquals(8, lecturerList.size());
+        lecturerList.forEach(System.out::println);
     }
 
     @Test
     void count() {
+        for (int i = 0; i < 120; i++) {
+            Lecturer lecturer = new Lecturer("Kasun Sampath",
+                    "Senior Trainer",
+                    "BSc (Hons) in Computing",
+                    LecturerType.FULL_TIME,
+                    0);
+            repository.save(lecturer);
+        }
+        long count = repository.count();
+
+        assertTrue(count >= 120);
     }
 }
