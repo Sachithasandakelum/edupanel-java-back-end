0c1c096890be0b6dc803ef2c654feeba89130499
Sachitha Abesinghe <sachithasandakelumsl@gmail.com>
2024-01-16 21:04:22 +0530

    Add CrudRepositoryimpl

--- src/main/java/lk/ijse/dep11/edupanel/repository/custom/impl/LecturerRepositoryImpl.java
+++ src/main/java/lk/ijse/dep11/edupanel/repository/custom/impl/LecturerRepositoryImpl.java
@@ -10,46 +10,5 @@ import java.util.List;
 import java.util.Optional;
 
 public class LecturerRepositoryImpl implements LecturerRepository {
-    private EntityManager em;
 
-    @Override
-    public void setEntityManager(EntityManager em) {
-        this.em = em;
-    }
-
-    @Override
-    public Lecturer save(Lecturer entity) {
-        em.persist(entity);
-        return entity;
-    }
-
-    @Override
-    public void update(Lecturer entity) {
-        em.merge(entity);
-    }
-
-    @Override
-    public void deleteById(Integer pk) {
-        em.remove(em.find(Lecturer.class, pk));
-    }
-
-    @Override
-    public boolean existsById(Integer pk) {
-        return findById(pk).isPresent();
-    }
-
-    @Override
-    public Optional<Lecturer> findById(Integer pk) {
-        return Optional.ofNullable(em.find(Lecturer.class, pk));
-    }
-
-    @Override
-    public List<Lecturer> findAll() {
-        return em.createQuery("SELECT l FROM Lecturer l", Lecturer.class).getResultList();
-    }
-
-    @Override
-    public long count() {
-        return em.createQuery("SELECT COUNT(l) FROM Lecturer l", Long.class).getSingleResult();
-    }
 }
